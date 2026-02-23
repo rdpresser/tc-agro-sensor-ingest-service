a856a6b8f0fcabfb1221958ec38541d423940357
Ricardo <ricardocavalcantesilva@gmail.com>
2026-02-23 16:41:15 -0300

    fix: ajuste do migration

--- src/Adapters/Outbound/TC.Agro.SensorIngest.Infrastructure/Migrations/20260223192222_InitialMigration.cs
+++ src/Adapters/Outbound/TC.Agro.SensorIngest.Infrastructure/Migrations/20260223192222_InitialMigration.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace TC.Agro.SensorIngest.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class InitialMigration : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
